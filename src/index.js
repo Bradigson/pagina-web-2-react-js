caab44f11c61daf95aa516d16038b45d82923c4a
Bradigson <71954800+Bradigson@users.noreply.github.com>
2022-02-10 00:58:12 -0400

    route remove

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
+
       <App/>
-    </Router>
+
   </React.StrictMode>,
   document.getElementById('root')
 );
